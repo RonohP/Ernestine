a010f49cc42463ca6f457ec80d883b26193bf9f0
Josephine Machage <Josephinemachage71@gmail.com>
2021-06-27 20:41:48 +0300

    my features

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,11 +1,66 @@
 import React from "react";
+import "./login.css";
 
 const Login = () => {
   return (
-    <div>
-      <p style={{ color: "white" }}>This is login</p>
+    <div class="grid-container">
+      <div class="grid-item item1">
+        <i class="fas fa-user-plus fa-5x"></i>
+        <p style={{ fontSize: "52px" }}>STUDENT HUB</p>
+      </div>
+      <div class="grid-item">
+        <div class="block">
+          <a type="button" href="/register" class="btn btn-dark dark1">Sign Up</a>
+        </div>
+        <div class="block1">
+          <p style={{ fontSize: "16px" }}>Welcome</p>
+          <p>Log in to access university class booking services </p>
+        </div>
+        <div class="block2">
+          Social Networks
+</div>
+      </div>
+      <div class="grid-item">
+        <div class="block3">
+          <a type="button" href="Login.jsx" class="btn btn1">Sign In</a>
+        </div>
+        <div class="block4">
+          <form>
+            <div class="input-container">
+              <input class="input-field" type="email" placeholder="Email" />
+              <i class="fa fa-envelope icon"></i>
+            </div>
+            <div class="input-container">
+              <input class="input-field" type="password" placeholder="Password" />
+              <i class="fa fa-key icon"></i>
+            </div>
+            <div class="input-container" style={{ marginLeft: "310px" }}>
+              <button type="submit" class="btn btn1">Log in<i class="fa fa-arrow-right icon1"></i>
+              </button>
+
+            </div>
+          </form>
+        </div>
+        <div class="block5">
+          <ul>
+            <li>
+              <i class="fab fa-facebook-f fa-x"></i>
+            </li>
+            <li>
+              <i class="fab fa-twitter fa-x"></i>
+            </li>
+            <li>
+              <i class="fab fa-github fa-x"></i>
+            </li>
+            <li>
+              <i class="fab fa-linkedin-in fa-x"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-};
+}
+
 
 export default Login;
--- src/Pages/register/Register.jsx
+++ src/Pages/register/Register.jsx
@@ -3,41 +3,66 @@ import "./register.css";
 
 function Register() {
   return (
-    <div className="container">
-      <div className="register">
-        <div className="container">
-          <h1>Register</h1>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <button>Register</button>
-          <hr />
-          <p>Or Sign up With</p> <hr />
+    <div class="grid-container">
+      <div class="grid-item item1">
+        <i class="fas fa-user-plus fa-5x"></i>
+        <p style={{ fontSize: "52px" }}>STUDENT HUB</p>
+      </div>
+      <div class="grid-item">
+        <div class="block">
+          <a type="button" href="/register" class="btn btn1">Sign Up</a>
+        </div>
+        <div class="block1">
+          <p style={{ fontSize: "16px" }}>Welcome</p>
+          <p>Fill the form to have access to the portal </p>
+        </div>
+        <div class="block2">
+          Social Networks
+</div>
+      </div>
+      <div class="grid-item">
+        <div class="block3">
+          <a type="button" href="/login" class="btn btn-dark dark1">Sign In</a>
+        </div>
+        <div class="block4">
+          <form>
+            <div class="input-container">
+              <input class="input-field" type="email" placeholder="Email" />
+              <i class="fa fa-envelope icon"></i>
+            </div>
+            <div class="input-container">
+              <input class="input-field" type="password" placeholder="Password" />
+              <i class="fa fa-key icon"></i>
+            </div>
+            <div class="input-container">
+              <input class="input-field" type="password" placeholder="Confirm Password" />
+              <i class="fa fa-key icon"></i>
+            </div>
+            <label>
+              <input type="checkbox" value="" name="remember" style={{ marginBottom: "15px" }} /> By creating an account you agree to our <a href="#" style={{ color: "#fc804a" }}>Terms & Privacy</a>.
+    </label>
+            <div class="input-container" style={{ marginLeft: "310px" }}>
+              <button type="submit" class="btn btn1">Register<i class="fa fa-arrow-right icon1"></i>
+              </button>
+
+            </div>
+          </form>
+        </div>
+        <div class="block5">
           <ul>
             <li>
-              <i className="fab fa-facebook-f fa-2x"></i>
+              <i class="fab fa-facebook-f fa-x"></i>
             </li>
             <li>
-              <i className="fab fa-twitter fa-2x"></i>
+              <i class="fab fa-twitter fa-x"></i>
             </li>
             <li>
-              <i className="fab fa-github fa-2x"></i>
+              <i class="fab fa-github fa-x"></i>
             </li>
             <li>
-              <i className="fab fa-linkedin-in fa-2x"></i>
+              <i class="fab fa-linkedin-in fa-x"></i>
             </li>
           </ul>
-          <span className="copyright"> Ernestine &copy; 2021</span>
-        </div>
-      </div>
-      <div className="login">
-        <div className="container">
-          <i className="fas fa-user-plus fa-5x"></i>
-          <h2>Welcome</h2>
-          <p>Already a member, proceed to login</p>
-          <button>
-            Login <i className="fas fa-arrow-circle-right"></i>
-          </button>
         </div>
       </div>
     </div>
