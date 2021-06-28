a47541a84174858d53a16baa631b25797cb98838
Nazarene Gena <genarene96@gmail.com>
2021-06-28 22:00:19 +0300

    redesigned the schedule

--- src/Pages/register/Register.jsx
+++ src/Pages/register/Register.jsx
@@ -1,16 +1,94 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+// import useForm from "../../useForm";
+import validate from "../../validateInfo";
 import "./register.css";
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const password = useRef({});
+  password.current = watch("password", "");
+  const handleRegister = (data) => {
+    console.log(data);
+  };
+
   return (
     <div class="container">
       <div class="register">
-        <div class="container">
+        <form class="container" onSubmit={handleSubmit(handleRegister)}>
           <h1>Register</h1>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <button>Register</button>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Enter a valid e-mail address",
+              },
+            })}
+          />
+          {errors.email && (
+            <p
+              style={{
+                color: "red",
+                fontWeight: "550",
+                fontFamily: "cursive",
+              }}
+            >
+              {errors.email.message}
+            </p>
+          )}
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            {...register("password", {
+              required: "You must specify a password",
+              minLength: {
+                value: 8,
+                message: "Password must have at least 6 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <p
+              style={{
+                color: "red",
+                fontWeight: "550",
+                fontFamily: "cursive",
+              }}
+            >
+              {errors.password.message}
+            </p>
+          )}
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            {...register("confirmPassword", {
+              validate: (value) =>
+                value === password.current || "The passwords do not match",
+            })}
+          />
+          {errors.confirmPassword && (
+            <p
+              style={{
+                color: "red",
+                fontWeight: "550",
+                fontFamily: "cursive",
+              }}
+            >
+              {errors.confirmPassword.message}
+            </p>
+          )}
+          <button type="submit">Register</button>
           <hr />
           <p>Or Sign up With</p> <hr />
           <ul>
@@ -28,17 +106,17 @@ function Register() {
             </li>
           </ul>
           <span class="copyright"> Ernestine &copy; 2021</span>
-        </div>
+        </form>
       </div>
       <div class="login">
-        <div class="container">
+        <form class="container">
           <i class="fas fa-user-plus fa-5x"></i>
           <h2>Welcome</h2>
           <p>Already a member, proceed to login</p>
           <button>
             Login <i class="fas fa-arrow-circle-right"></i>
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
